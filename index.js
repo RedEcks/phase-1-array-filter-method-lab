1341556cf77a8e87d9f7a6ebd1304fc60dffea65
Xavier Dunkley <xavierxv18@gmail.com>
2022-02-15 14:57:34 -0500

    commit tree d9dc3f0d782f5a05147aab18e2a7a45671f2ad8b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase()===name.toLowerCase());
+
+}
+
+function fuzzyMatch(drivers,firstInitials){
+    return drivers.filter(driver=>driver.substring(0,2)===firstInitials)
+}
+
+function matchName(drivers,name){
+    return drivers.filter(driver=> driver.name===name)
+}
